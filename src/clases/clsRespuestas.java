799df7156d5fd9248fdecb49ee7fc103f9af465f
joisRomero <joseluis0651@gmail.com>
2021-10-30 15:26:42 -0500

    interfaz de iniciar sesion creada

--- src/clases/clsRespuestas.java
+++ src/clases/clsRespuestas.java
@@ -3,13 +3,15 @@ package clases;
 public class clsRespuestas {
     private String titulo;
     private String respuesta;
+    private int idPostulacion;
 
     public clsRespuestas() {
     }
 
-    public clsRespuestas(String titulo, String respuesta) {
+    public clsRespuestas(String titulo, String respuesta, int idPostulacion) {
         this.titulo = titulo;
         this.respuesta = respuesta;
+        this.idPostulacion = idPostulacion;
     }
 
     public String getTitulo() {
@@ -27,6 +29,12 @@ public class clsRespuestas {
     public void setRespuesta(String respuesta) {
         this.respuesta = respuesta;
     }
-    
-    
+
+    public int getIdPostulacion() {
+        return idPostulacion;
+    }
+
+    public void setIdPostulacion(int idPostulacion) {
+        this.idPostulacion = idPostulacion;
+    } 
 }
